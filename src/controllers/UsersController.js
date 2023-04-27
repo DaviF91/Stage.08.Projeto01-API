8406ee424ec0f36e16c1be7b2dffb05189204bb4
Davi Ferreira <daviferreiraaew@gmail.com>
2023-04-27 17:35:15 -0300

    Atualizando usuaro

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,12 +6,11 @@
     * delete = DELETE para remover um registro.
 */
 const {hash} = require("bcryptjs");
-
 const AppError = require('../utils/AppError');
-
 const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
+  //create users
   async create(request, response){
     const {name, email, password} = request.body;
     const database = await sqliteConnection();
@@ -35,6 +34,39 @@ class UsersController {
 
   }
 
+  //update users
+  async update(request,response){
+    const {name, email} = request.body;
+    const {id} = request.params;
+
+    const database = await  sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user){
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError("Este email já está em uso");
+    }
+
+    user.name = name;
+    user.email = email;
+
+    //atualiza na tabela de usuáros e defina os valores
+    await database.run(`
+    UPDATE users SET
+    name = ?, 
+    email = ?,
+    updated_at = ?
+    WHERE id = ?`,
+    [user.name, user.email, new Date(), id]
+    );
+
+    return response.json();
+  }
 }
 
 module.exports = UsersController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,12 +3,13 @@ const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
 
-const userRoutes = Router()
+const userRoutes = Router();
 
 //instancia
-const usersController = new UsersController
+const usersController = new UsersController;
 
 
-userRoutes.post("/", usersController.create)
+userRoutes.post("/", usersController.create);
+userRoutes.put("/:id", usersController.update);
 
 module.exports = userRoutes //exportanto o arquivo para quem quiser utilizar
