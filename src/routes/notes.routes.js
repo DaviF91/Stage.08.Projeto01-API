cfc0a170378c1a57f42517e8f9025ac7dda50462
Davi Ferreira <daviferreiraaew@gmail.com>
2023-06-05 17:36:27 -0300

    deletando nota

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -11,6 +11,7 @@ const notesController = new NotesController;
 
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show);
+notesRoutes.delete("/:id", notesController.delete);
 
 
 module.exports = notesRoutes; //exportanto o arquivo para quem quiser utilizar
